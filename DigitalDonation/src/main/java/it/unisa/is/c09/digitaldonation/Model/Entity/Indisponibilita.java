3a26c9172828571baa408685b03d62883e0c41ef
Kevin Pacifico <k.pacifico@studenti.unisa.it>
2021-12-28 02:37:17 +0100

    UpdateIndisponibilita

--- DigitalDonation/src/main/java/it/unisa/is/c09/digitaldonation/Model/Entity/Indisponibilita.java
+++ DigitalDonation/src/main/java/it/unisa/is/c09/digitaldonation/Model/Entity/Indisponibilita.java
@@ -1,8 +1,7 @@
 package it.unisa.is.c09.digitaldonation.Model.Entity;
 
-import javax.persistence.Entity;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
+import java.util.Date;
 
 /**
  * @author Kevin Pacifico, Elpidio Mazza
@@ -10,15 +9,95 @@ import javax.persistence.ManyToOne;
  */
 @Entity
 public class Indisponibilita {
-    @ManyToOne
-    @JoinColumn(name = "indisponibilita_codice_fiscale")
-    private Donatore indisponibilita;
 
-    public Donatore getIndisponibilita() {
-        return indisponibilita;
+    @Id
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "donatore")
+    private String codiceFiscaleDonatore;
+    private Date dataProssimaDisponibilita;
+    private String motivazioni;
+    private String nomeMedico;
+
+    /**
+     * Costruttore che crea un oggetto Donazione vuoto,
+     * che verra' popolato con i metodi setters.
+     */
+    public Indisponibilita(){}
+
+    /**
+     * Costruttore di un'Indisponibilita' con parametri utili nei casi di test.
+     * @param codiceFiscaleDonatore è il codice fiscale del donatore.
+     * @param dataProssimaDisponibilita è la data di fine indisponibilità.
+     * @param motivazioni sono le motivazioni di indisponibilità.
+     * @param nomeMedico è il nome del medico che ha effettuato la visita.
+     */
+    public Indisponibilita(String codiceFiscaleDonatore, Date dataProssimaDisponibilita, String motivazioni, String nomeMedico) {
+        this.codiceFiscaleDonatore = codiceFiscaleDonatore;
+        this.dataProssimaDisponibilita = dataProssimaDisponibilita;
+        this.motivazioni = motivazioni;
+        this.nomeMedico = nomeMedico;
     }
 
-    public void setIndisponibilita(Donatore indisponibilita) {
-        this.indisponibilita = indisponibilita;
+    /**
+     * Metodo che ritorna il codice fiscale del donatore.
+     * @return codiceFiscaleDonatore e' il codice fiscale del donatore.
+     */
+    public String getCodiceFiscaleDonatore() {
+        return codiceFiscaleDonatore;
+    }
+
+    /**
+     * Metodo che setta il codice fiscale del donatore.
+     * @param codiceFiscaleDonatore e' il codice fiscale del donatore.
+     */
+    public void setCodiceFiscaleDonatore(String codiceFiscaleDonatore) {
+        this.codiceFiscaleDonatore = codiceFiscaleDonatore;
+    }
+
+    /**
+     * Metodo che ritorna la data della prossima disponibilità a donare.
+     * @return dataProssimaDisponibilita e' la data della prossima disponibilità a donare.
+     */
+    public Date getDataProssimaDisponibilita() {
+        return dataProssimaDisponibilita;
+    }
+
+    /**
+     * Metodo che setta la data della prossima disponibilità a donare.
+     * @param dataProssimaDisponibilita e' la data della prossima disponibilità a donare.
+     */
+    public void setDataProssimaDisponibilita(Date dataProssimaDisponibilita) {
+        this.dataProssimaDisponibilita = dataProssimaDisponibilita;
+    }
+
+    /**
+     * Metodo che ritorna le motivazioni dell'indisponibilità a donare.
+     * @return motivazioni sono le motivazioni dell'indisponibilità a donare.
+     */
+    public String getMotivazioni() {
+        return motivazioni;
+    }
+
+    /**
+     * Metodo che setta le motivazioni dell'indisponibilità a donare.
+     * @param motivazioni sono le motivazioni dell'indisponibilità a donare.
+     */
+    public void setMotivazioni(String motivazioni) {
+        this.motivazioni = motivazioni;
+    }
+
+    /**
+     * Metodo che ritorna il nome del medico che ha effettuato la visita.
+     * @return nomeMedico e' il nome del medico che ha effettuato la visita.
+     */
+    public String getNomeMedico() {
+        return nomeMedico;
+    }
+
+    /**
+     * Metodo che setta il nome del medico che ha effettuato la visita.
+     * @param nomeMedico è il nome del medico che ha effettuato la visita.
+     */
+    public void setNomeMedico(String nomeMedico) {
+        this.nomeMedico = nomeMedico;
     }
 }
