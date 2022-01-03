5f237ff3d5faf97f3210e9721a33d5dbe2480555
Mattia Sapere <mattiasapere84@gmail.com>
2022-01-03 17:45:57 +0100

    Realizzazione di tutti i form.

--- DigitalDonation/src/main/java/it/unisa/is/c09/digitaldonation/Utils/Forms/AutodichiarazioneIndisponibilitaForm.java
+++ DigitalDonation/src/main/java/it/unisa/is/c09/digitaldonation/Utils/Forms/AutodichiarazioneIndisponibilitaForm.java
@@ -1,5 +1,69 @@
 package it.unisa.is.c09.digitaldonation.Utils.Forms;
 
+import org.springframework.format.annotation.DateTimeFormat;
+import java.util.Date;
+
 public class AutodichiarazioneIndisponibilitaForm {
-    //Date dataProssimaDisponibilita, String motivazioni, String nomeMedico
+
+    public AutodichiarazioneIndisponibilitaForm(Date dataProssimaDisponibilita, String motivazioni, String nomeMedico){
+        this.dataProssimaDisponibilita = dataProssimaDisponibilita;
+        this.motivazioni = motivazioni;
+        this.nomeMedico = nomeMedico;
+    }
+
+    /**
+     * Metodo che ritorna la data di disponbilità dell'autodichiarazione di indisponibilità.
+     *
+     * @return dataProssimaDisponibilita e' la data di disponbilità dell'autodichiarazione di indisponibilità.
+     */
+    public Date getDataProssimaDisponibilita() {
+        return dataProssimaDisponibilita;
+    }
+    /**
+     * Metodo che setta la data di disponbilità dell'autodichiarazione di indisponibilità.
+     *
+     * @param dataProssimaDisponibilita e' la data di disponbilità dell'autodichiarazione di indisponibilità.
+     */
+    public void setDataProssimaDisponibilita(Date dataProssimaDisponibilita) {
+        this.dataProssimaDisponibilita = dataProssimaDisponibilita;
+    }
+
+    /**
+     * Metodo che ritorna le motivazioni dell'autodichiarazione di indisponibilità.
+     *
+     * @return motivazioni sono le motivazioni dell'autodichiarazione di indisponibilità.
+     */
+    public String getMotivazioni() {
+        return motivazioni;
+    }
+    /**
+     * Metodo che setta le motivazioni dell'autodichiarazione di indisponibilità.
+     *
+     * @param motivazioni sono le motivazioni dell'autodichiarazione di indisponibilità.
+     */
+    public void setMotivazioni(String motivazioni) {
+        this.motivazioni = motivazioni;
+    }
+
+    /**
+     * Metodo che ritorna il nome del medico dell'autodichiarazione di indisponibilità.
+     *
+     * @return nomeMedico è il nome del medico dell'autodichiarazione di indisponibilità.
+     */
+    public String getNomeMedico() {
+        return nomeMedico;
+    }
+    /**
+     * Metodo che setta il nome del medico dell'autodichiarazione di indisponibilità.
+     *
+     * @param nomeMedico è il nome del medico dell'autodichiarazione di indisponibilità.
+     */
+    public void setNomeMedico(String nomeMedico) {
+        this.nomeMedico = nomeMedico;
+    }
+
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+    private Date dataProssimaDisponibilita;
+    private String motivazioni;
+    private String nomeMedico;
 }
--- DigitalDonation/src/main/java/it/unisa/is/c09/digitaldonation/Utils/Forms/AutodichiarazioneIndisponibilitaFormValidate.java
+++ DigitalDonation/src/main/java/it/unisa/is/c09/digitaldonation/Utils/Forms/AutodichiarazioneIndisponibilitaFormValidate.java
@@ -1,4 +1,15 @@
 package it.unisa.is.c09.digitaldonation.Utils.Forms;
 
+import org.springframework.stereotype.Component;
+
+/**
+ * Classe che definisce un validatore per {@link AutodichiarazioneIndisponibilitaForm}.
+ * Il controllo effettuato rigurda la
+ * validità di alcuni campi definiti nell'entità Indisponibilità.
+ *
+ * @author Mattia Sapere
+ */
+
+@Component
 public class AutodichiarazioneIndisponibilitaFormValidate {
 }
