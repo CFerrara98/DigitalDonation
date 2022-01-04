19274100b95a36fdfcf8d1e97a6aa3bb1bf441c2
marikaspagnazito <m.spagnazito1@studenti.unisa.it>
2022-01-04 11:32:37 +0100

    Gestione Tesserino Service
    Mancano: aggiorna Tesserino ( non sappiamo se deve essere lasciato in TesserinoService oppure bisogna spostarlo in GestioneSeduteDonazioni) e manca generaPassword

--- DigitalDonation/src/main/java/it/unisa/is/c09/digitaldonation/GestioneTesserinoManagement/GestioneTesserinoService.java
+++ DigitalDonation/src/main/java/it/unisa/is/c09/digitaldonation/GestioneTesserinoManagement/GestioneTesserinoService.java
@@ -1,7 +1,133 @@
 package it.unisa.is.c09.digitaldonation.GestioneTesserinoManagement;
 
+import it.unisa.is.c09.digitaldonation.ErroreManagement.OrganizzazioneSeduteError.CannotSaveDataRepositoryException;
+import it.unisa.is.c09.digitaldonation.ErroreManagement.OrganizzazioneSeduteError.CannotUpdateDataRepositoryException;
+import it.unisa.is.c09.digitaldonation.Model.Entity.Tesserino;
+import it.unisa.is.c09.digitaldonation.Model.Entity.*;
+import it.unisa.is.c09.digitaldonation.Model.Repository.IndisponibilitaRepository;
+import it.unisa.is.c09.digitaldonation.Model.Repository.SedutaRepository;
+import it.unisa.is.c09.digitaldonation.Model.Repository.TesserinoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Date;
+import java.util.List;
 
 @Service
-public class GestioneTesserinoService {
+public class GestioneTesserinoService implements GestioneTesserinoServiceInterface {
+
+
+
+    @Autowired
+    private TesserinoRepository tesserinoRepository;
+
+    @Autowired
+    private IndisponibilitaRepository indisponibilitaRepository;
+
+    @Autowired
+    private SedutaRepository sedutaRepository;
+
+
+    /**
+     * Questo metodo permette all'operatore di creare un nuovo tesserino.
+     *
+     * @param tesserino Il tesserino
+     * @return il tesserino creato
+     */
+    @Override
+    @Transactional(rollbackFor = Exception.class)
+    public Tesserino creazioneTesserino(Tesserino tesserino) throws CannotSaveDataRepositoryException {
+        if(tesserino == null){
+            throw new CannotSaveDataRepositoryException("tesserinoError", "Errore, il tesserino è null");
+        }else if(tesserinoRepository.findTesserinoByIdTessera(tesserino.getIdTessera())!=null){
+            throw new CannotSaveDataRepositoryException("tesserinoError", "Il tesserino già esiste");
+        }
+        tesserinoRepository.save(tesserino);
+        return tesserino;
+    }
+
+
+    /**
+     * Questo metodo permette di dichiarare l'indisponibilità di un donatore a donare
+     *
+     * @param data la data di disponibilità
+     * @param motivazione motivazione di indisponibilità
+     * @return l'indisponibilità
+     */
+    @Override
+    @Transactional(rollbackFor = Exception.class)
+    public Indisponibilita autodichiarazioneIndisponibilita(Date data, String motivazione, String codiceFiscaleDonatore) throws CannotSaveDataRepositoryException {
+
+        List<Seduta> listaSedute;
+        Indisponibilita indisponibilita = new Indisponibilita();
+        if (data == null){
+            throw new CannotSaveDataRepositoryException("autodichiarazioneError", "Errore, la data è null");
+        }
+        else if(motivazione == null){
+            throw new CannotSaveDataRepositoryException("autodichiarazioneError", "Errore, la motivazione è null");
+        }
+
+        indisponibilita.setDataProssimaDisponibilita(data);
+        indisponibilita.setMotivazioni(motivazione);
+        indisponibilita.setCodiceFiscaleDonatore(codiceFiscaleDonatore);
+        indisponibilitaRepository.save(indisponibilita);
+
+        listaSedute = sedutaRepository.findAll();
+        for(Seduta s: listaSedute){
+            if(s.getDataSeduta().before(indisponibilita.getDataProssimaDisponibilita())){
+                for(int i = 0; i < s.getListaDonatore().size(); i++){
+                    if(s.getListaDonatore().get(i).getCodiceFiscale().equals(codiceFiscaleDonatore)){
+                        s.getListaDonatore().remove(i);
+                        i--;
+                    }
+                }
+                sedutaRepository.save(s);
+            }
+        }
+
+
+        return indisponibilita;
+
+    }
+
+
+    /**
+     * Questo metodo permette di aggiornare il tesserino
+     *
+     * @param tesserino Il tesserino
+     * @return il tesserino aggiornato
+     */
+    @Override
+    @Transactional(rollbackFor = Exception.class)
+    public Tesserino aggiornaTesserino(Tesserino tesserino) throws CannotUpdateDataRepositoryException {
+
+
+
+    }
+
+
+
+
+
+
+    /**
+     * Questo metodo permette di generare una password in seguito alla creazione del tesserino
+     *
+     * @param tesserino Il tesserino
+     * @return la password generata
+     */
+    @Override
+    @Transactional(rollbackFor = Exception.class)
+    public String generaPassword(Tesserino tesserino) throws CannotSaveDataRepositoryException {
+
+        if(tesserino == null){
+            throw new CannotSaveDataRepositoryException("tesserinoError", "Errore, il tesserino è null");
+        }
+
+    }
+
+
+
+
 }
--- DigitalDonation/src/main/java/it/unisa/is/c09/digitaldonation/GestioneTesserinoManagement/GestioneTesserinoServiceInterface.java
+++ DigitalDonation/src/main/java/it/unisa/is/c09/digitaldonation/GestioneTesserinoManagement/GestioneTesserinoServiceInterface.java
@@ -1,5 +1,7 @@
 package it.unisa.is.c09.digitaldonation.GestioneTesserinoManagement;
 
+import it.unisa.is.c09.digitaldonation.ErroreManagement.OrganizzazioneSeduteError.CannotSaveDataRepositoryException;
+import it.unisa.is.c09.digitaldonation.ErroreManagement.OrganizzazioneSeduteError.CannotUpdateDataRepositoryException;
 import it.unisa.is.c09.digitaldonation.Model.Entity.Indisponibilita;
 import it.unisa.is.c09.digitaldonation.Model.Entity.Tesserino;
 
@@ -7,11 +9,11 @@ import java.util.Date;
 
 public interface GestioneTesserinoServiceInterface {
 
-    public Tesserino creazioneTesserino(Tesserino tesserino);
+    public Tesserino creazioneTesserino(Tesserino tesserino) throws CannotSaveDataRepositoryException;
 
-    public Indisponibilita autodichiarazioneIndisponibilita(Date data);
+    public Indisponibilita autodichiarazioneIndisponibilita(Date data, String motivazione, String codiceFiscaleDonatore) throws CannotSaveDataRepositoryException;
 
-    public Tesserino aggiornaTesserino(Tesserino tesserino);
+    public Tesserino aggiornaTesserino(Tesserino tesserino) throws CannotUpdateDataRepositoryException;
 
-    public String generaPassword(Tesserino tesserino);
+    public String generaPassword(Tesserino tesserino) throws CannotSaveDataRepositoryException;
 }
