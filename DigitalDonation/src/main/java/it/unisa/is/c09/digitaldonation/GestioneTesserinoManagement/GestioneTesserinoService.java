d1b4ce47c8962bb28719de152e430a1048e5d697
fabbbooh <f.siepe1@studenti.unisa.it>
2022-01-06 10:50:10 +0100

    Commit in piu

--- DigitalDonation/src/main/java/it/unisa/is/c09/digitaldonation/GestioneTesserinoManagement/GestioneTesserinoService.java
+++ DigitalDonation/src/main/java/it/unisa/is/c09/digitaldonation/GestioneTesserinoManagement/GestioneTesserinoService.java
@@ -194,7 +194,6 @@ public class GestioneTesserinoService implements GestioneTesserinoServiceInterfa
                 return codiceFiscale;
             }
         }
-        return codiceFiscale;
     }
 
 
